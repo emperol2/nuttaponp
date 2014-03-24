31b435786a2216346851f727d65d7fd35212e468
Nuttapon <emperol2@gmil.com>
2014-03-24 13:07:28 +0700

    add Agent Contact CRM

--- Nexa/test_nonEN_chars.rb
+++ Nexa/test_nonEN_chars.rb
@@ -319,6 +319,58 @@ class TestNonEN < Test::Unit::TestCase
 
   end
 
+  def test_new_Agent_Contact_CRM
+    # To change this template use File | Settings | File Templates.
+    @driver.navigate.to(@base_url)
+
+    login_CRM()
+    #@driver.find_element(:link_text, 'Contacts').click
+    #@driver.find_element(:link_text, 'Add a New Contact').click
+    @driver.navigate.to('https://216.46.31.242/Nexa/index.html?_flowId=manageContactCrm-flow&_initialViewId=AddContactCrm')
+
+    getAllInputFields = @driver.find_elements(:tag_name, 'input')
+    getAllInputFields.each_with_index do |x, index|
+      #puts index
+      if index >= 5
+
+        getType = x.attribute('type')
+        getName = x.attribute('name')
+
+        if getType == 'text' and (getName.include? '[' or getName.include? ']' or getName.include? 'end' or getName.include? 'stop' or getName.include? 'followUpReason' or getName.include? 'dailyOptionInterval') == false
+
+          nonEn = ['ç', 'ÿ', 'ï', 'è', 'è', 'ç', 'ê', 'ô', 'ü', 'â', 'ñ']
+          @randomNumber = Random.new()
+          @randOneChar = (@randomNumber.rand * (nonEn.length - 1)).round
+          puts @randOneChar
+          @getChar = nonEn[@randOneChar]
+
+          @attrName = x.attribute('name')
+
+          if @attrName.include? 'start'
+            puts 'has calendar'
+          else
+            @attrName = @attrName.split('.')
+            @attrName = @attrName[1]
+            @randOneNum = ((@randomNumber.rand * 200).round) + 1
+            @concatStr = @attrName + ' test' + @randOneNum.to_s + ' ' + @getChar
+            puts @concatStr
+            x.send_keys @concatStr
+          end
+        end
+      end
+
+    end
+
+    @driver.find_element(:id, 'btnSave').click
+    sleep 3
+
+  end
+
+
+
+
+
+
   def login
     @driver.find_element(:id, 'txtUsername').send_keys 'nuttapon'
     @driver.find_element(:id, 'txtPassword').send_keys '12345'
